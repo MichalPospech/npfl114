ce7b54116a0ff0b1932084d9a0328a397ee85c34
Michal Pospěch <michal.pospech@gmail.com>
2019-04-04 22:56:43 +0200

    Finished mnist_multiple assignment

--- labs/05/mnist_multiple.py
+++ labs/05/mnist_multiple.py
@@ -5,56 +5,80 @@ import tensorflow as tf
 from mnist import MNIST
 
 # The neural network model
+
+# Could not create cudnn handle: CUDNN_STATUS_INTERNAL_ERROR without following lines, solution from https://github.com/tensorflow/tensorflow/issues/24496#issuecomment-464909727
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+
 class Network:
     def __init__(self, args):
-        # TODO: Add a `self.model` which has two inputs, both images of size [MNIST.H, MNIST.W, MNIST.C].
-        # It then passes each input image through the same network (with shared weights), performing
-        # - convolution with 10 filters, 3x3 kernel size, stride 2, "valid" padding, ReLU activation
-        # - convolution with 20 filters, 3x3 kernel size, stride 2, "valid" padding, ReLU activation
-        # - flattening layer
-        # - fully connected layer with 200 neurons and ReLU activation
-        # obtaining a 200-dimensional feature representation of each image.
-        #
-        # Then, it produces three outputs:
-        # - classify the computed representation of the first image using a densely connected layer
-        #   into 10 classes;
-        # - classify the computed representation of the second image using the
-        #   same connected layer (with shared weights) into 10 classes;
-        # - concatenate the two image representations, process them using another fully connected
-        #   layer with 200 neurons and ReLU, and finally compute one output with tf.nn.sigmoid
-        #   activation (the goal is to predict if the first digit is larger than the second)
-        #
-        # Train the outputs using SparseCategoricalCrossentropy for the first two inputs
-        # and BinaryCrossentropy for the third one, utilizing Adam with default arguments.
-        pass
+        conv1 = tf.keras.layers.Conv2D(10, (3, 3), 2, "valid",activation = tf.keras.activations.relu)
+        conv2 = tf.keras.layers.Conv2D(20, (3, 3), 2, "valid",activation = tf.keras.activations.relu)
+        flat = tf.keras.layers.Flatten()
+        full = tf.keras.layers.Dense(200, activation=tf.keras.activations.relu)
 
+        def create_submodel(input_layer):
+            return full(flat(conv2(conv1(input_layer))))
+
+        in_layer1 = tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C])
+        in_layer2 = tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C])
+        sub1 = create_submodel(in_layer1)
+        sub2 = create_submodel(in_layer2)
+        num_pred = tf.keras.layers.Dense(
+            10, activation=tf.keras.activations.softmax)
+        num_pred1 = num_pred(sub1)
+        num_pred2 = num_pred(sub2)
+        concat = tf.keras.layers.concatenate([sub1, sub2])
+        full = tf.keras.layers.Dense(
+            200, activation=tf.keras.activations.relu)(concat)
+        out = tf.keras.layers.Dense(
+            1, activation=tf.keras.activations.sigmoid)(full)
+        model = tf.keras.Model(inputs=[in_layer1, in_layer2], outputs=[
+                               num_pred1, out, num_pred2])
+        model.compile(tf.keras.optimizers.Adam(), loss=[tf.keras.losses.SparseCategoricalCrossentropy(
+        ), tf.keras.losses.BinaryCrossentropy(), tf.keras.losses.SparseCategoricalCrossentropy()])
+        self.model = model
     @staticmethod
     def _prepare_batches(batches_generator):
         batches = []
         for batch in batches_generator:
             batches.append(batch)
             if len(batches) >= 2:
-                # TODO: yield the suitable modified inputs and targets using batches[0:2]
+                model_inputs = [batches[0]["images"], batches[1]["images"]]
+                model_targets = [batches[0]["labels"],(batches[0]
+                                 ["labels"] > batches[1]["labels"])*1, batches[1]["labels"]]
                 yield (model_inputs, model_targets)
                 batches.clear()
 
     def train(self, mnist, args):
         for epoch in range(args.epochs):
-            # TODO: Train for one epoch using `model.train_on_batch` for each batch.
             for batch in self._prepare_batches(mnist.train.batches(args.batch_size)):
-                pass
+                self.model.train_on_batch(batch[0], batch[1])
 
             # Print development evaluation
-            print("Dev {}: directly predicting: {:.4f}, comparing digits: {:.4f}".format(epoch + 1, *self.evaluate(mnist.dev, args)))
+            print("Dev {}: directly predicting: {:.4f}, comparing digits: {:.4f}".format(
+                epoch + 1, *self.evaluate(mnist.dev, args)))
 
     def evaluate(self, dataset, args):
-        # TODO: Evaluate the given dataset, returning two accuracies, the first being
-        # the direct prediction of the model, and the second computed by comparing predicted
-        # labels of the images.
-        for inputs, targets in self._prepare_batches(dataset.batches(args.batch_size)):
-            pass
 
-        return direct_accuracy, indirect_accuracy
+        num_samples =0
+        correct_direct = 0
+        correct_indirect = 0
+        for inputs, targets in self._prepare_batches(dataset.batches(args.batch_size)):
+            num1, direct, num2 = self.model.predict(inputs)
+            num_samples+=len(direct)
+            num1 = np.argmax(num1,axis=1)
+            num2 = np.argmax(num2, axis=1)
+            indirect = (num1>num2)*1
+            direct_pred = (direct >= 0.5)*1
+            direct_comp = direct_pred==np.reshape(targets[1],direct_pred.shape) #WTF NumPy, why is the reshape needed?
+            correct_direct += np.sum(direct_comp)
+            correct_indirect += np.sum(indirect == targets[1])
+        return correct_direct/num_samples, correct_indirect/num_samples
 
 
 if __name__ == "__main__":
@@ -65,17 +89,22 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-    parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
-    parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--batch_size", default=50,
+                        type=int, help="Batch size.")
+    parser.add_argument("--epochs", default=10, type=int,
+                        help="Number of epochs.")
+    parser.add_argument("--recodex", default=False,
+                        action="store_true", help="Evaluation in ReCodEx.")
+    parser.add_argument("--threads", default=1, type=int,
+                        help="Maximum number of threads to use.")
     args = parser.parse_args()
 
     # Fix random seeds and number of threads
     np.random.seed(42)
     tf.random.set_seed(42)
     if args.recodex:
-        tf.keras.utils.get_custom_objects()["glorot_uniform"] = lambda: tf.keras.initializers.glorot_uniform(seed=42)
+        tf.keras.utils.get_custom_objects(
+        )["glorot_uniform"] = lambda: tf.keras.initializers.glorot_uniform(seed=42)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
@@ -83,7 +112,8 @@ if __name__ == "__main__":
     args.logdir = os.path.join("logs", "{}-{}-{}".format(
         os.path.basename(__file__),
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+        ",".join(("{}={}".format(re.sub(
+            "(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
 
     # Load the data
@@ -94,4 +124,5 @@ if __name__ == "__main__":
     network.train(mnist, args)
     with open("mnist_multiple.out", "w") as out_file:
         direct, indirect = network.evaluate(mnist.test, args)
-        print("{:.2f} {:.2f}".format(100 * direct, 100 * indirect), file=out_file)
+        print("{:.2f} {:.2f}".format(
+            100 * direct, 100 * indirect), file=out_file)
