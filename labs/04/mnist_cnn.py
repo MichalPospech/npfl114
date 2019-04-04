21b9940ae022c19ed05ab082028e2a9469a2a24c
Michal Pospěch <michal.pospech@gmail.com>
2019-04-04 20:40:42 +0200

    Completed mnist_Cnn assignment

--- labs/04/mnist_cnn.py
+++ labs/04/mnist_cnn.py
@@ -1,54 +1,94 @@
 #!/usr/bin/env python3
 import numpy as np
 import tensorflow as tf
-
+from re import compile, match
 from mnist import MNIST
-
+from functools import reduce
 # The neural network model
+# Could not create cudnn handle: CUDNN_STATUS_INTERNAL_ERROR without following lines, solution from https://github.com/tensorflow/tensorflow/issues/24496#issuecomment-464909727
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
 class Network(tf.keras.Model):
     def __init__(self, args):
         inputs = tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C])
 
-        # TODO: Add CNN layers specified by `args.cnn`, which contains
-        # comma-separated list of the following layers:
-        # - `C-filters-kernel_size-stride-padding`: Add a convolutional layer with ReLU
-        #   activation and specified number of filters, kernel size, stride and padding.
-        # - `CB-filters-kernel_size-stride-padding`: Same as `C`, but use batch normalization.
-        #   In detail, start with a convolutional layer without bias and activation,
-        #   then add batch normalization layer, and finally ReLU activation.
-        # - `M-kernel_size-stride`: Add max pooling with specified size and stride.
-        # - `R-[layers]`: Add a residual connection. The `layers` contain a specification
-        #   of at least one convolutional layer (but not a recursive residual connection `R`).
-        #   The input to the specified layers is then added to their output.
-        # - `F`: Flatten inputs. Must appear exactly once in the architecture.
-        # - `D-hidden_layer_size`: Add a dense layer with ReLU activation and specified size.
-        # Produce the results in variable `hidden`.
+        def split_params(params):
+            split = compile(r"(R-\[[^\]]+\]|[^,]+),?")
+            groups = split.findall(params)
+            return groups
 
-        # Add the final output layer
-        outputs = tf.keras.layers.Dense(MNIST.LABELS, activation=tf.nn.softmax)(hidden)
+        def create_layer(desc):
+            if desc[0] == "F":
+                return tf.keras.layers.Flatten()
+            elif desc[0] == "D":
+                size = int(desc[2:])
+                return tf.keras.layers.Dense(size, tf.keras.activations.relu)
+            elif desc[0] == "M":
+                regex =  compile(r"M-(.+)-(.+)")
+                match_ob = regex.match(desc)
+                return tf.keras.layers.MaxPool2D(int(match_ob[1]),int(match_ob[2]))
+                
+            elif desc[0:2] == "CB":
+                regex = compile(r"CB-(.+)-(.+)-(.+)-(.+)")
+                match_ob = regex.match(desc)
+                conv = tf.keras.layers.Conv2D(int(match_ob[1]),int(match_ob[2]),int(match_ob[3]),match_ob[4],use_bias=False)
+                batch_norm = tf.keras.layers.BatchNormalization()
+                activ = tf.keras.layers.Activation(tf.keras.activations.relu)
+                layers = [conv,batch_norm,activ]
+                return lambda x: apply_layers([x]+ layers)
+            elif desc[0] == "C":
+                regex = compile(r"C-(.+)-(.+)-(.+)-(.+)")
+                match_ob = regex.match(desc)
+                return tf.keras.layers.Conv2D(int(match_ob[1]),int(match_ob[2]),int(match_ob[3]),match_ob[4], activation=tf.keras.activations.relu)
+            elif desc[0] == "R":
+                layers = process_params(desc[3:-1])
+                return lambda x: tf.keras.layers.add([x,layers(x)])
+                
+        def apply_layers(layers):
+            return reduce(lambda beg, layer: layer(beg), layers)
+        
+        def process_params(params):
+            layers_desc = split_params(params)
+            layers =  [create_layer(desc) for desc in layers_desc]
+            return(lambda x: apply_layers([x] + layers))
+
+
+        network_ctor = process_params(args.cnn)
+        hidden = network_ctor(inputs)        # Add the final output layer
+        outputs = tf.keras.layers.Dense(
+            MNIST.LABELS, activation=tf.nn.softmax)(hidden)
 
         super().__init__(inputs=inputs, outputs=outputs)
 
         self.compile(
             optimizer=tf.keras.optimizers.Adam(),
             loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-            metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+            metrics=[tf.keras.metrics.SparseCategoricalAccuracy(
+                name="accuracy")],
         )
 
-        self.tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, update_freq=1000, profile_batch=1)
+        self.tb_callback = tf.keras.callbacks.TensorBoard(
+            args.logdir, update_freq=1000, profile_batch=1)
         self.tb_callback.on_train_end = lambda *_: None
 
     def train(self, mnist, args):
         self.fit(
             mnist.train.data["images"], mnist.train.data["labels"],
             batch_size=args.batch_size, epochs=args.epochs,
-            validation_data=(mnist.dev.data["images"], mnist.dev.data["labels"]),
+            validation_data=(
+                mnist.dev.data["images"], mnist.dev.data["labels"]),
             callbacks=[self.tb_callback],
         )
 
     def test(self, mnist, args):
-        test_logs = self.evaluate(mnist.test.data["images"], mnist.test.data["labels"], batch_size=args.batch_size)
-        self.tb_callback.on_epoch_end(1, dict(("val_test_" + metric, value) for metric, value in zip(self.metrics_names, test_logs)))
+        test_logs = self.evaluate(
+            mnist.test.data["images"], mnist.test.data["labels"], batch_size=args.batch_size)
+        self.tb_callback.on_epoch_end(1, dict(
+            ("val_test_" + metric, value) for metric, value in zip(self.metrics_names, test_logs)))
         return test_logs[self.metrics_names.index("accuracy")]
 
 
@@ -60,18 +100,24 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-    parser.add_argument("--cnn", default=None, type=str, help="CNN architecture.")
-    parser.add_argument("--epochs", default=30, type=int, help="Number of epochs.")
-    parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--batch_size", default=50,
+                        type=int, help="Batch size.")
+    parser.add_argument("--cnn", default=None, type=str,
+                        help="CNN architecture.")
+    parser.add_argument("--epochs", default=30, type=int,
+                        help="Number of epochs.")
+    parser.add_argument("--recodex", default=False,
+                        action="store_true", help="Evaluation in ReCodEx.")
+    parser.add_argument("--threads", default=1, type=int,
+                        help="Maximum number of threads to use.")
     args = parser.parse_args()
 
     # Fix random seeds and number of threads
     np.random.seed(42)
     tf.random.set_seed(42)
     if args.recodex:
-        tf.keras.utils.get_custom_objects()["glorot_uniform"] = lambda: tf.keras.initializers.glorot_uniform(seed=42)
+        tf.keras.utils.get_custom_objects(
+        )["glorot_uniform"] = lambda: tf.keras.initializers.glorot_uniform(seed=42)
     tf.config.threading.set_inter_op_parallelism_threads(args.threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
@@ -79,7 +125,8 @@ if __name__ == "__main__":
     args.logdir = os.path.join("logs", "{}-{}-{}".format(
         os.path.basename(__file__),
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+        ",".join(("{}={}".format(re.sub(
+            "(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
 
     # Load the data
