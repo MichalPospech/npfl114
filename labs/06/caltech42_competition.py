42af24d00b02a209932904f674be79898aef84be
Michal Pospěch <michal.pospech@gmail.com>
2019-04-19 17:40:00 +0200

    Caltech competiton baseline completed

--- labs/06/caltech42_competition.py
+++ labs/06/caltech42_competition.py
@@ -1,46 +1,77 @@
 #!/usr/bin/env python3
 import numpy as np
 import tensorflow as tf
-import tensorflow_hub as tfhub # Note: you need to install tensorflow_hub
+import tensorflow_hub as tfhub  # Note: you need to install tensorflow_hub
 
-from caltech42 import Caltech42
+# Could not create cudnn handle: CUDNN_STATUS_INTERNAL_ERROR without following lines, solution from https://github.com/tensorflow/tensorflow/issues/24496#issuecomment-464909727
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+from caltech42_v import Caltech42
 
 # The neural network model
-class Network:
-    def __init__(self, args):
-        # TODO: You should define `self.model`. You should use the following layer:
-        #   mobilenet = tfhub.KerasLayer("https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/2", output_shape=[1280])
-        # The layer:
-        # - if given `trainable=True/False` to KerasLayer constructor, the layer weights
-        #   either are marked or not marked as updatable by an optimizer;
-        # - however, batch normalization regime is set independently, by `training=True/False`
-        #   passed during layer execution.
-        #
-        # Therefore, to not train the layer at all, you should use
-        #   mobilenet = tfhub.KerasLayer("https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/2", output_shape=[1280], trainable=False)
-        #   features = mobilenet(inputs, training=False)
-        # On the other hand, to fully train it, you should use
-        #   mobilenet = tfhub.KerasLayer("https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/2", output_shape=[1280], trainable=True)
-        #   features = mobilenet(inputs)
-        # where the `training` argument to `mobilenet` is passed automatically in that case.
-        #
-        # Note that a model with KerasLayer can currently be saved only using
-        #   tf.keras.experimental.export_saved_model(model, path, serving_only=True/False)
-        # where `serving_only` controls whether only prediction, or also training/evaluation
-        # graphs are saved. To again load the model, use
-        #   model = tf.keras.experimental.load_from_saved_model(path, {"KerasLayer": tfhub.KerasLayer})
+def eval_test(args, epoch):
+    prediction = network.predict(caltech42.test.data["images"], batch_size=args.batch_size)
+    with open(os.path.join(args.logdir, "caltech42_" + str(epoch+1) + "_test.txt"), "w", encoding="utf-8") as out_file:
+        for probs in prediction:
+            print(np.argmax(probs), file=out_file)  
+    np.save(os.path.join(args.logdir, "caltech42_" + str(epoch+1) + "_test.npy"), prediction)
 
-        self.tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, update_freq=1000, profile_batch=1)
+    prediction = network.predict(caltech42.dev.data["images"], batch_size=args.batch_size)
+    np.save(os.path.join(args.logdir, "caltech42_" + str(epoch+1) + "_dev.npy"), prediction)
+
+
+class Network(tf.keras.Model):
+    def __init__(self, args):
+        inputs = prev = tf.keras.layers.Input(shape=[224, 224, 3])
+
+        mobilenet = tfhub.KerasLayer(
+            "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/2", output_shape=[1280], trainable=True)
+        prev = mobilenet(prev)
+        prev = tf.keras.layers.Dense(96, activation=tf.nn.relu)(prev)
+        prev = tf.keras.layers.Dropout(0.5)(prev)
+        prev = tf.keras.layers.Dense(Caltech42.LABELS, activation=tf.nn.softmax)(prev)
+
+        super().__init__(inputs=inputs, outputs=prev)
+
+        self.compile(
+            optimizer=tf.keras.optimizers.Adam(0.0001),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+            # loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, label_smoothing=0.1),
+            # metrics=[tf.keras.metrics.CategoricalAccuracy(name="accuracy")],
+        )
+
+
+        def scheduler(epoch):
+            if epoch < 7:
+                return 0.0001
+            elif epoch < 15:
+                return 0.00005
+            elif epoch <25:
+                return 0.00001
+            else:
+                return 0.000005
+        
+        self.tb_callback = tf.keras.callbacks.TensorBoard(
+            args.logdir, update_freq=1000, profile_batch=0)
         self.tb_callback.on_train_end = lambda *_: None
+        self.epoch_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
+        self.log_callback = tf.keras.callbacks.LambdaCallback(None,lambda epoch, logs: eval_test(args, epoch))
+        
+        self.summary()
+
 
     def train(self, caltech42, args):
-        # TODO: Implement training
-        pass
-
-    def predict(self, caltech42, args):
-        # TODO: Implement prediction
-        pass
-
+        self.fit_generator(
+            caltech42.train.batches(args.batch_size),
+            epochs=args.epochs,
+            validation_data=(caltech42.dev.data["images"], caltech42.dev.data["labels"]),
+            callbacks=[self.tb_callback,self.epoch_callback, self.log_callback]
+        )
 
 if __name__ == "__main__":
     import argparse
@@ -49,11 +80,14 @@ if __name__ == "__main__":
     import re
 
     # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
-    parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-    args = parser.parse_args()
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--batch_size", default = 64,
+                        type = int, help = "Batch size.")
+    parser.add_argument("--epochs", default = 1024,
+                        type = int, help = "Number of epochs.")
+    parser.add_argument("--threads", default = 0, type = int,
+                        help = "Maximum number of threads to use.")
+    args=parser.parse_args()
 
     # Fix random seeds
     np.random.seed(42)
@@ -62,20 +96,21 @@ if __name__ == "__main__":
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
     # Create logdir name
-    args.logdir = os.path.join("logs", "{}-{}-{}".format(
+    args.logdir=os.path.join("logs", "{}-{}-{}".format(
         os.path.basename(__file__),
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+        ",".join(("{}={}".format(re.sub(
+            "(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     ))
 
     # Load data
-    caltech42 = Caltech42()
+    caltech42=Caltech42(one_hot=False)
 
     # Create the network and train
-    network = Network(args)
+    network=Network(args)
     network.train(caltech42, args)
 
     # Generate test set annotations, but in args.logdir to allow parallel execution.
-    with open(os.path.join(args.logdir, "caltech42_competition_test.txt"), "w", encoding="utf-8") as out_file:
-        for probs in network.predict(caltech42.test, args):
-            print(np.argmax(probs), file=out_file)
+    # with open(os.path.join(args.logdir, "caltech42_competition_test.txt"), "w", encoding = "utf-8") as out_file:
+    #     for probs in network.predict(caltech42.test, args):
+    #         print(np.argmax(probs), file = out_file)
